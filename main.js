6a479e4611a50d76cb2de31ba6f329ac8429be33
Nurmuhammad2010271 <938630762a@gmail.com>
2025-03-02 10:46:15 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+let rang = prompt("Body ni riniang kiriting");
+let textrangi = prompt("Text kriting");
+let newrang = rang;
+let newtextrangi = textrangi;
+document.body.style.color = textrangi;
+document.body.style.backgroundColor = rang;
+document.body.style.fontSize = "2rem"
+document.body.append(newrang,newtextrangi);
